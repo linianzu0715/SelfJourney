36baac6753787f4253e282b29c731d7bc1449158
linianzu <linianzu@meituan.com>
2021-09-10 13:54:05 +0800

    初始

--- UpdateJobStatus.py
+++ UpdateJobStatus.py
@@ -1,13 +1,25 @@
 import fileinput
 
 def finishJob(JobNumber):
-    EXP_GODEN = alter("Storage/Job.csv", JobNumber)
+    EXP_GODEN = alter("Storage/Job.csv", JobNumber, "0","1")
     EXP = EXP_GODEN[0]
     GODEN = EXP_GODEN[1]
-    print("要增加的经验值为"+str(EXP))
-    print("要增加的金币为" + str(GODEN))
+    if EXP != 0:
+        print("要增加的经验值为"+str(EXP))
+    if GODEN != 0:
+        print("要增加的金币为" + str(GODEN))
 
-def alter(file,JobNumber):
+
+def unfinishJOb(JobNumber):
+    EXP_GODEN = alter("Storage/Job.csv", JobNumber, "1","0")
+    EXP = EXP_GODEN[0]
+    GODEN = EXP_GODEN[1]
+    if EXP != 0:
+        print("要减少的经验值为"+str(EXP))
+    if GODEN != 0:
+        print("要减少的金币为" + str(GODEN))
+
+def alter(file,JobNumber,oldStatus,newStatsu):
     """
     替换文件中的字符串
     :param file:文件名
@@ -21,13 +33,15 @@ def alter(file,JobNumber):
     with open(file, "r") as f:
         for line in f:
             line_list = line.strip("\n").split(",")
-            if line_list[0] == str(JobNumber) and line_list[5] == "0":
+            if line_list[0] == str(JobNumber) and line_list[5] == oldStatus:
                 EXP = line_list[3]
                 GODEN = line_list[4]
-                line_list[5] = "1"
+                line_list[5] = newStatsu
                 newLine = ",".join(line_list)
                 line = line.replace(line, newLine) + "\n"
-                print(line)
+                print("恭喜你完成任务：")
+                print("No：" + line_list[0] + " 任务名称：《" + line_list[1] + "》")
+                print("任务状态更新完成\n")
             file_data += line
     with open(file,"w") as f:
         f.write(file_data)
@@ -38,4 +52,8 @@ def alter(file,JobNumber):
 
 
 if __name__ == '__main__':
+    #如果要完成某个JOB，就在方法中填入JOBID，否则就填入0
     finishJob(1)
+
+    #如果要将JOB回退为未完成状态，就在方法中填入JOBID，否则就填入0
+    #unfinishJOb(1)
