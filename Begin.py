e38762a4bdda9d646f8f8a92e61e60b711aea10f
linianzu <linianzu@meituan.com>
2021-09-10 11:51:28 +0800

    初始

--- Begin.py
+++ Begin.py
@@ -8,7 +8,18 @@ def CheckPlayerInformation():
     while line:
         print(line.strip("\n"))
         line = f.readline()
+    f.close()
+
+def CheckJob():
+    print("当前未完成的任务：")
+    with open("Storage/Job.csv") as file:
+        for line in file:
+            # 只打印出状态为未完成的状态
+            line_list = line.strip("\n").split(",")
+            if line_list[0] == "任务编号" or line_list[5] == "0":
+                print(line.strip("\n"))
 
 if __name__ == '__main__':
     HelloGamePlayer()
     CheckPlayerInformation()
+    CheckJob()
